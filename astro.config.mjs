bc80e16cfe2dc32605c04e79029df0aa5a282d3f
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2025-03-15 09:07:52 +0000

    add Prettier configuration and update project files for formatting consistency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-    redirects: {
-        '/sngrcreative': 'https://sngrcreative.pages.dev'
-    }
-});
+  redirects: {
+    '/sngrcreative': 'https://sngrcreative.pages.dev'
+  }
+})
